b9e3f7abf225b6490ff2929e3fb947c5fc1d8f55
David Zhu <dzhu@192-168-1-9.tpgi.com.au>
2012-08-19 21:04:25 +1000

    realtime stock/commodities ticker - json converter

---
+++ web/src/main/scala/JsonConvertor.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzhu
+ * Date: 19/08/12
+ * Time: 8:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class JsonConvertor {
+
+}
---
+++ web/src/test/scala/JsonConvertorTest.scala
@@ -0,0 +1,12 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzhu
+ * Date: 19/08/12
+ * Time: 8:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class JsonConvertorTest extends FunSuite {
+
+}
